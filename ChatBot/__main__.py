0b276c69bcd57e0f9d2b9143fe8746802c6d1f50
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2025-03-12 20:45:16 +0530

    Update __main__.py

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,18 +1,23 @@
+import os
 import importlib
-import subprocess 
+import subprocess
 
 from pyrogram import idle
-from ChatBot import app
+
 from ChatBot.modules import ALL_MODULES
+from config import WEB_APP  
 
 async def boot():
     await app.start()
-    subprocess.Popen(['python3', 'app.py'])
+
     for module in ALL_MODULES:
         importlib.import_module(f"ChatBot.modules.{module}")
+
+    if WEB_APP:
+        subprocess.Popen(['python3', 'app.py'])
 
     await idle()
     await app.stop()
 
 if __name__ == "__main__":
-    app.run(boot())    
+    app.run(boot())
